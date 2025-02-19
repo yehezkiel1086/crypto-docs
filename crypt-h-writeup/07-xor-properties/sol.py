93cc404726a175046000a30a389469736f2f8059
yehezkiel1086 <tsukikagemaikaka1@gmail.com>
2025-02-19 20:57:47 +0700

    feat(crypt-h): update 07-xor-properties

--- crypt-h-writeup/07-xor-properties/sol.py
+++ crypt-h-writeup/07-xor-properties/sol.py
@@ -8,12 +8,12 @@ from pwn import xor
 # FLAG ^ KEY1 ^ KEY3 ^ KEY2 = 04ee9855208a2cd59091d04767ae47963170d1660df7f56f5faf
 
 k1 = bytes.fromhex("a6c8b6733c9b22de7bc0253266a3867df55acde8635e19c73313")
-a = bytes.fromhex("37dcb292030faa90d07eec17e3b1c6d8daf94c35d4c9191a5e1e")
-b = bytes.fromhex("c1545756687e7573db23aa1c3452a098b71a7fbf0fddddde5fc1")
-c = bytes.fromhex("04ee9855208a2cd59091d04767ae47963170d1660df7f56f5faf")
+k21 = bytes.fromhex("37dcb292030faa90d07eec17e3b1c6d8daf94c35d4c9191a5e1e")
+k23 = bytes.fromhex("c1545756687e7573db23aa1c3452a098b71a7fbf0fddddde5fc1")
+fk132 = bytes.fromhex("04ee9855208a2cd59091d04767ae47963170d1660df7f56f5faf")
 
-k2 = xor(k1, a)
-k3 = xor(k2, b)
-flag = xor(xor(k2, xor(c, k1)), k3)
+k2 = xor(k1, k21)
+k3 = xor(k2, k23)
+f = xor(xor(xor(fk132, k1), k3), k2)
 
-print(flag)
+print(f)
