428c88726a38961795913ed6b022768f9036adf3
yehezkiel1086 <tsukikagemaikaka1@gmail.com>
2025-02-19 20:43:27 +0700

    feat(crypt-h): edit 06-xor-starter

--- crypt-h-writeup/06-xor-starter/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# build image
-docker build . -t python3
-
-# run container
-docker run python3
--- crypt-h-writeup/06-xor-starter/sol.py
+++ crypt-h-writeup/06-xor-starter/sol.py
@@ -1,7 +1,7 @@
 #!/bin/python3
 
-from pwn import xor
-
 enc = "label"
-xor_enc = "".join([chr(ord(xor(x, 13))) for x in enc])
-print("crypto{" + xor_enc + "}")
+
+enc_bin = "".join([chr(ord(x) ^ 13) for x in enc])
+
+print("crypto{" + enc_bin + "}")
