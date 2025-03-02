e748796dcfc8ec312abdc4afa7d2e4c980025823
yehezkiel1086 <tsukikagemaikaka1@gmail.com>
2025-03-02 21:47:10 +0700

    feat(crypt-h): update 00-intro/09-xor-final

--- crypt-h-writeup/00-intro/09-xor-final/sol.py
+++ crypt-h-writeup/00-intro/09-xor-final/sol.py
@@ -8,8 +8,8 @@ from pwn import xor
 
 enc = bytes.fromhex("0e0b213f26041e480b26217f27342e175d0e070a3c5b103e2526217f27342e175d0e077e263451150104")
 
-enc_key = xor(enc, "crypto{".encode())
+# key = xor(enc, "crypto{") -- the key is "myXORkey"
 
-flag = xor(enc, "myXORkey".encode())
+flag = xor("myXORkey", enc)
 
 print(flag)
